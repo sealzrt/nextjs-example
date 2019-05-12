4e37e91f889742fbba035679abc908a324d028f7
zhangruitao-home <381596533@qq.com>
2019-05-12 15:10:32 +0800

    更新demo

--- chapter16-link/pages/index.js
+++ chapter16-link/pages/index.js
@@ -15,6 +15,9 @@ const as = {
  * 可以用 <Link> 组件实现客户端的路由切换
  * Link 是一个高阶组件
  * 可以使用<Link prefetch>使链接和预加载在后台同时进行，来达到页面的最佳性能
+ *
+ * Link组件 href 接收 URL 对象，而且它会自动格式化生成 URL 字符串
+ * as 替换路由, Link组件默认将新 url 推入路由栈中
  */
 export default () => (
     <div>
