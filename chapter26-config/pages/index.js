4db322751178669e7030ab61673fb3a60ce16f7d
张瑞涛 <zhangruitao@mmears.com>
2019-05-14 21:42:11 +0800

    update

---
+++ chapter26-config/pages/index.js
@@ -0,0 +1,14 @@
+/**
+ * 自动打包编译 (使用 webpack 和 babel)
+ * 热加载
+ * 以 ./pages作为服务端的渲染和索引
+ * @returns {*}
+ * @constructor
+ */
+const Index = () => (
+    <div>
+        <p>Hello Next.js</p>
+    </div>
+)
+
+export default Index
