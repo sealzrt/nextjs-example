c6b25039b86eb86f3d7b3b2d4373ed03c7facce6
张瑞涛 <zhangruitao@mmears.com>
2019-06-03 20:11:39 +0800

    add koa-compress

---
+++ chapter32-koa-compress/server.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const views = require('koa-views');
+const path = require('path');
+const static = require('koa-static');
+const compress = require('koa-compress');
+const app = new Koa();
+
+const port = parseInt(process.env.PORT, 10) || 3000
+
+// 加载模板引擎
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}));
+
+app.use(compress({
+  filter: function (content_type) {
+    return /text|javascript/i.test(content_type)
+  },
+  threshold: 2048,
+  flush: require('zlib').Z_SYNC_FLUSH
+}));
+
+app.use(static(
+  path.join(__dirname, './static')
+));
+
+app.use(async (ctx) => {
+  await ctx.render('index', {title: '我是首页'})
+});
+
+app.listen(port, () => {
+  console.log(`> Ready on http://localhost:${port}`)
+});
